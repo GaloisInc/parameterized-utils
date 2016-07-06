fdf260fe8a94a71b24a2976781726fa2ebb5743c
Eddy Westbrook <westbrook@galois.com>
2016-07-06 13:16:30 -0700

    Added the MonadNonce class, the NonceT transformer, and related types and operations

--- src/Data/Parameterized/Nonce.hs
+++ src/Data/Parameterized/Nonce.hs
@@ -19,6 +19,9 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RoleAnnotations #-}
 {-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Data.Parameterized.Nonce
   ( NonceGenerator
   , withIONonceGenerator
@@ -26,9 +29,19 @@ module Data.Parameterized.Nonce
   , freshNonce
   , Nonce
   , indexValue
+  , MonadNonce(..)
+  , NonceT(..)
+  , NonceST(..)
+  , NonceIO(..)
+  , getNonceSTGen
+  , runNonceST
+  , runNonceIO
   ) where
 
 import Control.Monad.ST
+import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.State
 import Data.Hashable
 import Data.IORef
 import Data.STRef
@@ -93,3 +106,44 @@ instance HashableF (Nonce s) where
 
 instance ShowF (Nonce s) where
   showF = show
+
+-- | A MonadNonce is a monad that can generate fresh 'None's in a given set
+-- (where we view the phantom type parameter of 'Nonce' as a designator of the
+-- set that the 'Nonce' came from)
+class Monad m => MonadNonce m where
+  type NonceSet m :: *
+  freshNonceM :: forall (tp :: k) . m (Nonce (NonceSet m) tp)
+
+-- | This transformer adds a nonce generator to a given monad
+newtype NonceT s m a =
+  NonceT { runNonceT :: ReaderT (NonceGenerator m s) m a }
+  deriving (Functor, Applicative, Monad)
+
+instance MonadTrans (NonceT s) where
+  lift m = NonceT $ lift m
+
+instance Monad m => MonadNonce (NonceT s m) where
+  type NonceSet (NonceT s m) = s
+  freshNonceM = NonceT $ lift . freshNonce =<< ask
+
+instance MonadNonce m => MonadNonce (StateT s m) where
+  type NonceSet (StateT s m) = NonceSet m
+  freshNonceM = lift $ freshNonceM
+
+-- | Helper type to build a 'MonadNonce' from the 'ST' monad
+type NonceST t s = NonceT s (ST t)
+
+-- | Helper type to build a 'MonadNonce' from the 'IO' monad
+type NonceIO s = NonceT s IO
+
+-- | Return the actual 'NonceGenerator' used in an 'ST' computation
+getNonceSTGen :: NonceST t s (NonceGenerator (ST t) s)
+getNonceSTGen = NonceT ask
+
+-- | Run a 'NonceST' computation with a fresh 'NonceGenerator'
+runNonceST :: (forall t s. NonceST t s a) -> a
+runNonceST m = runST $ withSTNonceGenerator $ runReaderT $ runNonceT m
+
+-- | Run a 'NonceIO' computation with a fresh 'NonceGenerator' inside 'IO'
+runNonceIO :: (forall s. NonceIO s a) -> IO a
+runNonceIO m = withIONonceGenerator $ runReaderT $ runNonceT m
