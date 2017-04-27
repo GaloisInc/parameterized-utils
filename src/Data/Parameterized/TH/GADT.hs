e5674b7211b9c70d047df4eb7961f1b0cd1ae0e5
Eric Mertens <emertens@galois.com>
2017-04-27 10:19:55 -0700

    Use the th-abstraction package

--- src/Data/Parameterized/TH/GADT.hs
+++ src/Data/Parameterized/TH/GADT.hs
@@ -7,7 +7,6 @@
 -- This module declares template Haskell primitives so that it is easier
 -- to work with GADTs that have many constructors.
 ------------------------------------------------------------------------
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE DoAndIfThenElse #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -22,11 +21,10 @@ module Data.Parameterized.TH.GADT
   , structuralHash
   , PolyEq(..)
     -- * Template haskell utilities that may be useful in other contexts.
-  , DataD(..)
+  , DataD
   , lookupDataType'
   , asTypeCon
-  , NormalizedCon(..)
-  , normalizeCon
+  , conPat
   , TypePat(..)
   , dataParamTypes
   , assocTypePats
@@ -34,11 +32,11 @@ module Data.Parameterized.TH.GADT
 
 import Control.Monad
 import Data.Hashable (hashWithSalt)
-import Data.List
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Language.Haskell.TH
+import Language.Haskell.TH.Datatype
 
 
 import Data.Parameterized.Classes
@@ -46,96 +44,28 @@ import Data.Parameterized.Classes
 ------------------------------------------------------------------------
 -- Template Haskell utilities
 
-tyVarName :: TyVarBndr -> Name
-tyVarName (PlainTV nm) = nm
-tyVarName (KindedTV nm _) = nm
+type DataD = DatatypeInfo
 
-data DataD = DD { _dataCtx :: Cxt
-                , _dataName :: Name
-                , dataTyVarBndrs :: [TyVarBndr]
-                , dataCtors :: [NormalizedCon]
-                } deriving (Show)
-
-data NormalizedCon = NC
-  { nrmlConName   :: Name
-  , nrmlConFields :: [Type]
-  }
-  deriving (Show)
-
-asTyConI :: Info -> Maybe Dec
-asTyConI (TyConI d) = Just d
-asTyConI _ = Nothing
-
-normalizeCon :: Type -> Con -> Either String [NormalizedCon]
-normalizeCon t c =
-  case c of
-    NormalC n xs      -> pure [NC n (map snd xs)]
-    RecC n xs         -> pure [NC n [x | (_,_,x) <- xs ]]
-    InfixC l n r      -> pure [NC n [snd l, snd r]]
-    ForallC _ _ c'    -> normalizeCon t c'
-#if MIN_VERSION_template_haskell(2,11,0)
-    GadtC ns xs t'    -> traverse (gadtCase (map snd xs) t t') ns
-    RecGadtC ns xs t' -> traverse (gadtCase [x | (_,_,x) <- xs] t t') ns
-
-gadtCase :: [Type] -> Type -> Type -> Name -> Either String NormalizedCon
-gadtCase fields t t' n = NC n <$> traverse subst fields
-  where
-    typeMapping = [ (x,y) | (VarT x, VarT y) <- zip (unappsT t') (unappsT t) ]
-
-    subst (VarT x)        = case lookup x typeMapping of
-                              Nothing -> pure WildCardT
-                              Just y  -> pure (VarT y)
-    subst ForallT{}       = Left "Unable to normalize field types with ForallT"
-    subst (AppT f x)      = AppT    <$> subst f <*> subst x
-    subst (SigT x y)      = SigT    <$> subst x <*> subst y
-    subst (InfixT l c r)  = InfixT  <$> subst l <*> pure c <*> pure r
-    subst (UInfixT l c r) = InfixT  <$> subst l <*> pure c <*> pure r
-    subst (ParensT x)     = ParensT <$> subst x
-    subst x               = pure x
-
-unappsT :: Type -> [Type]
-unappsT t = reverse (go t)
-  where
-    go (AppT f x) = x : go f
-    go x          = [x]
-#endif
-
-tyVarBndrName :: TyVarBndr -> Name
-tyVarBndrName (PlainTV  n  ) = n
-tyVarBndrName (KindedTV n _) = n
-
-asDataD :: Dec -> Either String DataD
-#if MIN_VERSION_template_haskell(2,11,0)
-asDataD (DataD ctx n v _ ctors _d) =
-#else
-asDataD (DataD ctx n v ctors _d) =
-#endif
-  do let ty = foldl AppT (ConT n) (map (VarT . tyVarBndrName) v)
-     nctors <- normalizeCon ty `traverse` ctors
-     pure DD
-       { _dataCtx = ctx
-       , _dataName = n
-       , dataTyVarBndrs = v
-       , dataCtors = concat nctors
-       }
-asDataD _ = Left "asDataD: Expected name of data type"
+lookupDataType' :: Name -> Q DatatypeInfo
+lookupDataType' = reifyDatatype
 
 -- | Given a constructor and string, this generates a pattern for matching
 -- the expression, and the names of variables bound by pattern in order
 -- they appear in constructor.
-conPat :: NormalizedCon
-        -> String
-        -> Q (Pat, [Name])
-conPat (NC nm a) pre = do
-  nms <- replicateM (length a) (newName pre)
-  return (ConP nm (VarP <$> nms), nms)
+conPat ::
+  ConstructorInfo {- ^ constructor information -} ->
+  String          {- ^ generated name prefix   -} ->
+  Q (Pat, [Name]) {- ^ pattern and bound names -}
+conPat con pre = do
+  nms <- replicateM (length (constructorFields con)) (newName pre)
+  return (ConP (constructorName con) (VarP <$> nms), nms)
 
 
 -- | Return an expression corresponding to the constructor.
 -- Note that this will have the type of a function expecting
 -- the argumetns given.
-conExpr :: NormalizedCon -> Exp
-conExpr c = ConE (nrmlConName c)
+conExpr :: ConstructorInfo -> Exp
+conExpr = ConE . constructorName
 
 ------------------------------------------------------------------------
 -- TypePat
@@ -162,8 +92,8 @@ matchTypePat _ (ConType tpq) tp = do
   return (tp' == tp)
 matchTypePat _ _ _ = return False
 
-dataParamTypes :: DataD -> [Type]
-dataParamTypes d = VarT . tyVarName <$> dataTyVarBndrs d
+dataParamTypes :: DatatypeInfo -> [Type]
+dataParamTypes = datatypeVars
 
 -- | Find value associated with first pattern that matches given pat if any.
 assocTypePats :: [Type] -> [(TypePat,v)] -> Type -> Q (Maybe v)
@@ -177,25 +107,9 @@ assocTypePats dTypes ((p,v):pats) tp = do
 ------------------------------------------------------------------------
 -- Contructor cases
 
-typeVars :: Type -> Set Name
-typeVars tp = typeVars' tp Set.empty
+typeVars :: TypeSubstitution a => a -> Set Name
+typeVars = Set.fromList . freeVariables
 
-typeVars' :: Type -> Set Name -> Set Name
-typeVars' (ForallT vars _ tp) s =
-  foldl' (flip Set.delete) (typeVars' tp s) (tyVarName <$> vars)
-typeVars' (AppT x y) s = typeVars' x $! typeVars' y s
-typeVars' (SigT t _) s = typeVars' t s
-typeVars' (VarT nm) s = Set.insert nm s
-typeVars' _ s = s
-
-lookupDataType' :: Name -> Q DataD
-lookupDataType' tpName = do
-  info <- reify tpName
-  let dec = maybe (Left "not a type constructor") Right
-          $ asTyConI info
-  case asDataD =<< dec of
-    Left e   -> fail ("lookupDataType' " ++ show (ppr tpName) ++ ": " ++ e)
-    Right dd -> return dd
 
 -- | @declareStructuralEquality@ declares a structural equality predicate.
 structuralEquality :: TypeQ -> [(TypePat,ExpQ)] -> ExpQ
@@ -258,22 +172,22 @@ mkSimpleEqF dTypes bnd pats nm a = do
   match (pure (TupP [xp, yp])) (normalB rv) []
 
 -- | Match equational form.
-mkEqF :: DataD -- ^ Data declaration.
+mkEqF :: DatatypeInfo -- ^ Data declaration.
       -> [(TypePat,ExpQ)]
-      -> NormalizedCon
+      -> ConstructorInfo
       -> [MatchQ]
-mkEqF d pats (NC nm a) = (:[]) $ do
-  let dVars = tyVarName <$> dataTyVarBndrs d
-  let bnd | null dVars = Set.empty
-          | otherwise  = Set.fromList (init dVars)
-  mkSimpleEqF (VarT <$> dVars) bnd pats nm a
+mkEqF d pats con =
+  let dVars = datatypeVars d
+      bnd | null dVars = Set.empty
+          | otherwise  = typeVars (init dVars)
+  in [mkSimpleEqF dVars bnd pats (constructorName con) (constructorFields con)]
 
 -- | @structuralTypeEquality f@ returns a function with the type:
 --   forall x y . f x -> f y -> Maybe (x :~: y)
 structuralTypeEquality :: TypeQ -> [(TypePat,ExpQ)] -> ExpQ
 structuralTypeEquality tpq pats = do
-  d <- lookupDataType' =<< asTypeCon "structuralTypeEquality" =<< tpq
-  let trueEqs = concatMap (mkEqF d pats) (dataCtors d)
+  d <- reifyDatatype =<< asTypeCon "structuralTypeEquality" =<< tpq
+  let trueEqs = concatMap (mkEqF d pats) (datatypeCons d)
       baseEq  = match [p| (_, _)|] (normalB [| Nothing |]) []
   [| \x y -> $(caseE [| (x, y) |] (trueEqs ++ [baseEq])) |]
 
@@ -283,8 +197,8 @@ structuralTypeOrd :: TypeQ
                   -> [(TypePat,ExpQ)] -- ^ List of type patterns to match.
                   -> ExpQ
 structuralTypeOrd tpq l = do
-  d <- lookupDataType' =<< asTypeCon "structuralTypeEquality" =<< tpq
-  matchEqs <- traverse (mkOrdF d l) (dataCtors d)
+  d <- reifyDatatype =<< asTypeCon "structuralTypeEquality" =<< tpq
+  matchEqs <- traverse (mkOrdF d l) (datatypeCons d)
   case reverse matchEqs of
     [] -> do
       [| \_ _ -> EQF |]
@@ -366,11 +280,11 @@ mkSimpleOrdF dTypes pats nm a = do
          ]
 
 -- | Match equational form.
-mkOrdF :: DataD -- ^ Data declaration.
+mkOrdF :: DatatypeInfo -- ^ Data declaration.
        -> [(TypePat,ExpQ)] -- ^ Patterns for matching arguments
-       -> NormalizedCon
+       -> ConstructorInfo
        -> Q [MatchQ]
-mkOrdF d pats (NC nm a) = mkSimpleOrdF (dataParamTypes d) pats nm a
+mkOrdF d pats con = mkSimpleOrdF (datatypeVars d) pats (constructorName con) (constructorFields con)
 
 -- | Find the first recurseArg f var tp@ applies @f@ to @var@ where @var@ has type @tp@.
 recurseArg :: (Type -> Q (Maybe ExpQ))
@@ -392,11 +306,11 @@ recurseArg m f v tp = do
 -- the constructor @c@ and applies @f@ to each argument.
 traverseAppMatch :: (Type -> Q (Maybe ExpQ)) -- Pattern match function
                  -> ExpQ -- ^ Function to apply to each argument recursively.
-                 -> NormalizedCon -- ^ Constructor to match.
+                 -> ConstructorInfo -- ^ Constructor to match.
                  -> MatchQ -- ^ Match expression that
 traverseAppMatch pats fv c0 = do
   (pat,patArgs) <- conPat c0 "p"
-  exprs <- zipWithM (recurseArg pats fv) (varE <$> patArgs) (nrmlConFields c0)
+  exprs <- zipWithM (recurseArg pats fv) (varE <$> patArgs) (constructorFields c0)
 
   let mkRes :: ExpQ -> [(Name, Maybe Exp)] -> ExpQ
       mkRes e [] = e
@@ -424,12 +338,12 @@ traverseAppMatch pats fv c0 = do
 -- a traversal @f@ to the subterms with free variables in @tp@.
 structuralTraversal :: TypeQ -> [(TypePat, ExpQ)] -> ExpQ
 structuralTraversal tpq pats0 = do
-  d <- lookupDataType' =<< asTypeCon "structuralTraversal" =<< tpq
+  d <- reifyDatatype =<< asTypeCon "structuralTraversal" =<< tpq
   f <- newName "f"
   a <- newName "a"
   lamE [varP f, varP a] $
       caseE (varE a)
-      (traverseAppMatch (assocTypePats (dataParamTypes d) pats0) (varE f) <$> dataCtors d)
+      (traverseAppMatch (assocTypePats (datatypeVars d) pats0) (varE f) <$> datatypeCons d)
 
 asTypeCon :: Monad m => String -> Type -> m Name
 asTypeCon _ (ConT nm) = return nm
@@ -439,13 +353,13 @@ asTypeCon fn _ = fail $ fn ++ " expected type constructor."
 -- @Int -> tp -> Int@ that hashes type.
 structuralHash :: TypeQ -> ExpQ
 structuralHash tpq = do
-  d <- lookupDataType' =<< asTypeCon "structuralHash" =<< tpq
+  d <- reifyDatatype =<< asTypeCon "structuralHash" =<< tpq
   s <- newName "s"
   a <- newName "a"
   lamE [varP s, varP a] $
-    caseE (varE a) (zipWith (matchHashCtor (varE s)) [0..] (dataCtors d))
+    caseE (varE a) (zipWith (matchHashCtor (varE s)) [0..] (datatypeCons d))
 
-matchHashCtor :: ExpQ -> Integer  -> NormalizedCon -> MatchQ
+matchHashCtor :: ExpQ -> Integer  -> ConstructorInfo -> MatchQ
 matchHashCtor s0 i c = do
   (pat,vars) <- conPat c "x"
   let args = [| $(litE (IntegerL i)) :: Int |] : (varE <$> vars)
@@ -457,11 +371,11 @@ matchHashCtor s0 i c = do
 -- @tp -> ShowS@ that shows the constructor.
 structuralShowsPrec :: TypeQ -> ExpQ
 structuralShowsPrec tpq = do
-  d <- lookupDataType' =<< asTypeCon "structuralShowPrec" =<< tpq
+  d <- reifyDatatype =<< asTypeCon "structuralShowPrec" =<< tpq
   p <- newName "_p"
   a <- newName "a"
   lamE [varP p, varP a] $
-    caseE (varE a) (matchShowCtor (varE p) <$> dataCtors d)
+    caseE (varE a) (matchShowCtor (varE p) <$> datatypeCons d)
 
 showCon :: ExpQ -> Name -> Int -> MatchQ
 showCon p nm n = do
@@ -473,5 +387,5 @@ showCon p nm n = do
           | otherwise = [| showParen ($(p) >= 10) $(foldl go ctor vars) |]
   match (pure pat) (normalB rhs) []
 
-matchShowCtor :: ExpQ -> NormalizedCon -> MatchQ
-matchShowCtor p (NC nm tps) = showCon p nm (length tps)
+matchShowCtor :: ExpQ -> ConstructorInfo -> MatchQ
+matchShowCtor p con = showCon p (constructorName con) (length (constructorFields con))
