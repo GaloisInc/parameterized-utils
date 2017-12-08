0f5a6f49687164a88c54c222b0850615dbef5ce1
Rob Dockins <rdockins@galois.com>
2017-12-08 13:55:43 -0800

    Remove the unused FreeParamF module. Add a new `Data.Parameterized` module that brings most of the library into scope.

---
+++ src/Data/Parameterized.hs
@@ -0,0 +1,19 @@
+module Data.Parameterized
+( module Data.Parameterized.Classes       
+, module Data.Parameterized.Ctx           
+, module Data.Parameterized.TraversableF  
+, module Data.Parameterized.TraversableFC 
+, module Data.Parameterized.NatRepr       
+, module Data.Parameterized.Pair          
+, module Data.Parameterized.Some          
+, module Data.Parameterized.SymbolRepr    
+) where
+
+import Data.Parameterized.Classes
+import Data.Parameterized.Ctx
+import Data.Parameterized.TraversableF
+import Data.Parameterized.TraversableFC
+import Data.Parameterized.NatRepr
+import Data.Parameterized.Pair
+import Data.Parameterized.Some
+import Data.Parameterized.SymbolRepr    
