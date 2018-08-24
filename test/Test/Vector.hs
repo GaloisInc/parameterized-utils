ed5e679b9154527f1a10b87751591b9516212806
Langston Barrett <langston.barrett@gmail.com>
2018-08-24 11:45:15 -0700

    implement Vector.joinWithM, a monadic joinWith

--- test/Test/Vector.hs
+++ test/Test/Vector.hs
@@ -1,5 +1,9 @@
 {-# Language DataKinds #-}
+{-# Language ExplicitForAll #-}
+{-# Language TypeOperators #-}
+{-# Language TypeFamilies #-}
 {-# Language FlexibleInstances #-}
+{-# Language ScopedTypeVariables #-}
 {-# Language StandaloneDeriving #-}
 
 module Test.Vector
@@ -7,7 +11,7 @@ module Test.Vector
 ) where
 
 import Test.Tasty
-import Test.Tasty.QuickCheck
+import Test.Tasty.QuickCheck (Arbitrary(..), Gen, testProperty)
 
 import Data.Parameterized.NatRepr
 import Data.Parameterized.Vector
@@ -17,12 +21,26 @@ import Prelude hiding (reverse)
 instance KnownNat n => Arbitrary (NatRepr n) where
   arbitrary = return knownNat
 
+-- GHC thinks that this instances overlaps with the
+-- "Arbitrary a => Arbitrary (Maybe a)" instance from QuickCheck, but it doesn't:
+-- there is no "Arbitrary a => Arbitrary (Vector n a)".
+instance {-# OVERLAPS #-} forall a n. (1 <= n, Arbitrary a, KnownNat n)
+    => Arbitrary (Maybe (Vector n a)) where
+  arbitrary = do
+    n <- (arbitrary :: Gen (NatRepr n))
+    l <- (arbitrary :: Gen [a])
+    return $ fromList n l
+
+-- We use @Ordering@ just because it's simple
 vecTests :: IO TestTree
 vecTests = testGroup "Vector" <$> return
   [ testProperty "reverse100" $
-      \n v -> fromList (n :: NatRepr 100) (v :: [Int]) ==
+      \n v -> fromList (n :: NatRepr 100) (v :: [Ordering]) ==
               (reverse <$> (reverse <$> (fromList n v)))
   , testProperty "reverseSingleton" $
-      \n v -> fromList (n :: NatRepr 1) (v :: [Int]) ==
+      \n v -> fromList (n :: NatRepr 1) (v :: [Ordering]) ==
               (reverse <$> (fromList n v))
+  , testProperty "split-join" $
+      \n w v -> (v :: Maybe (Vector (5 * 5) Ordering)) ==
+                (join (n :: NatRepr 5) . split n (w :: NatRepr 5) <$> v)
   ]
