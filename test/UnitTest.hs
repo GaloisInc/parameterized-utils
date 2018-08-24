668b25a5f6cc7b2bdbc34aaec399ae26f251f616
Langston Barrett <langston.barrett@gmail.com>
2018-08-23 17:14:41 -0700

    Vector.reverse

--- test/UnitTest.hs
+++ test/UnitTest.hs
@@ -4,6 +4,7 @@ import Test.Tasty.Runners.AntXML
 
 import qualified Test.Context
 import qualified Test.NatRepr
+import qualified Test.Vector
 
 main :: IO ()
 main = tests >>= defaultMainWithIngredients ingrs
@@ -19,4 +20,5 @@ tests :: IO TestTree
 tests = testGroup "ParameterizedUtils" <$> sequence
   [ Test.Context.contextTests
   , Test.NatRepr.natTests
+  , Test.Vector.vecTests
   ]
