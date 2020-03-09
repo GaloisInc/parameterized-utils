b217641c90728849d93ad4396b2724b59646ec3f
Ben Selfridge <benselfridge@dyn-40-190.galois.com>
2020-03-09 11:34:19 -0700

    hashtable-linear: changes hashtable implementatino to use Linear rather than Cuckoo

--- src/Data/Parameterized/HashTable.hs
+++ src/Data/Parameterized/HashTable.hs
@@ -30,7 +30,7 @@ module Data.Parameterized.HashTable
 
 import Control.Applicative
 import Control.Monad.ST
-import qualified Data.HashTable.ST.Cuckoo as H
+import qualified Data.HashTable.ST.Linear as H
 import GHC.Exts (Any)
 import Unsafe.Coerce
 
